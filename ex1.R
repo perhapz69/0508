52070e07532352f21bfb911019568ee61ee6ce91
perhapz69 <perhapz69@gmail.com>
2020-05-29 02:14:11 +0000

    新增

--- ex1.R
+++ ex1.R
@@ -1,4 +1,6 @@
 filename='學12-1.新生註冊率-以「系(所)」統計.csv'
 x=read.csv(filename,fileEncoding = "utf8" )
-
+summary(x$當學年度新生註冊率...D..C..A.B...100.)
+boxplot(x$當學年度新生註冊率...D..C..A.B...100.)
+head(x[order(x$當學年度新生註冊率...D..C..A.B...100.),])
 
